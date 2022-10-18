093f3e851998aa40a5b9d736ca476fd6cef844c5
Marcel Weber <mrweber@gmx.ch>
2022-10-18 22:32:40 +0200

    Details added

--- pages/angebote.js
+++ pages/angebote.js
@@ -1,11 +1,33 @@
-function Page(){
 
-    const background = "https://cdn.pixabay.com/photo/2016/06/04/11/12/particles-1435363_960_720.jpg"
 
+    function Page(){
+        const background = "" // "https://cdn.pixabay.com/photo/2016/06/04/11/12/particles-1435363_960_720.jpg"
     return(
         <main className="main" style={{backgroundImage: `url("${background}")`}}>
             <section className="section">
                 <h1>Angebote</h1>
+                <h3>WordPress Webseite neu</h3>
+                    <h4>Aufdatieren</h4>
+    Inhalte prüfen
+    Suchmaschinenoptimierung
+    Sicherheit
+    Responsives Design
+    Datenschutz
+    Statistiken
+                     <p>
+        Sofern Hosting und Domain bereits vorhanden sind, gilt der Preis von
+        CHF500. Ansonsten kommt das Hosting und die Domain noch dazu. Der
+        Gesamtpreis ist dann abhängig vom gewählten Anbieter.
+      </p>
+      <h3>WordPress Webseite bestehend</h3>
+        <h3>Massgeschneiderte Lösungen</h3>
+      <p>
+        Je nach Wunsch muss der Preis individuell festgelegt werden. Eine
+        Webseite/Webapplikation von Hand zu bauen kostet sehr viel Zeit, kann
+        dafür aber zu 100% Ihren Wünschen entsprechen. Eine Webseite mit einem
+        anderen CMS als WordPress wäre wesentlich günstiger.
+      </p>
+                
                 <h3>Details</h3>
                 <p>Wenn möglich wird Schweizer Server- und Hostinginfrastruktur genutzt. Dies gilt vor allem für Seiten, die mit WordPRess gebaut werden.</p>
                 <p>Teilweise ist dies aber nicht möglich - sei es wegen fehlender Möglichkeiten seitens Hoster oder wegen hoher monatlichen Kosten für die notwendige Infrastruktur.</p>
