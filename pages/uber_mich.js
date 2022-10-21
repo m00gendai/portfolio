eb99e4db7768c449eeb89e9a00f66c766a22ddce
Marcel Weber <mrweber@gmx.ch>
2022-10-21 17:29:23 +0200

    expanded description

--- pages/uber_mich.js
+++ pages/uber_mich.js
@@ -7,7 +7,16 @@ function Page_About(){
     return(
         <main className="main" style={{backgroundImage: `url("${background}")`}}>
             <section className="section">
-                Über mich
+                <h2>Wer bin ich?</h2>
+                <p>Ein Hobby-Webentwickler aus dem westlichen Eck des nördlichsten Kantons, der seit 2015 Webseiten zusammenbastelt.</p>
+                <h2>Das kenne ich</h2>
+                HTML5 CSS3 JavaScript 
+                <h2>Das kann ich nicht</h2>
+                <ul>
+                    <li>Webshops selber bauen</li>
+                    <li>Amazon Web Services</li>
+                    <li>Komplexe 3D-Webseiten</li>
+                </ul>
             </section>
         </main>
     )
