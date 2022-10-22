9294ae26cdcf3f1faffbe52e63cb344d245c4193
Marcel Weber <mrweber@gmx.ch>
2022-10-22 18:12:19 +0200

    Footer changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,17 @@ import '../styles/globals.css'
 import s from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const date = new Date()
+  const foundingYear = "2022"
+  const currentYear = date.getFullYear()
+
   return (
   <>
   <Component_MainNavMenu />
   <Component {...pageProps} />
   <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-
-        </a>
+        © mrweber.ch {foundingYear == currentYear ? "2022" : `2022 - ${currentYear}`}
       </footer>
   </>
   )
