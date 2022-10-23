eb0fc2ba62766cbd4c49672efaa935b62476f4fe
Marcel Weber <mrweber@gmx.ch>
2022-10-23 17:48:07 +0200

    New background for projekte

--- pages/projekte.js
+++ pages/projekte.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css'
 import Reel from "../components/Reel"
 function Page_Websites(){
 
-    const background = "https://cdn.pixabay.com/photo/2013/02/15/10/58/wave-81840_960_720.jpg"
+    const background = "/hd-g5a621cc5b_1920.jpg"
 
     return(
         <main className="main" style={{backgroundImage: `url("${background}")`}}>
