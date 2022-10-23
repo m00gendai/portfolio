c401a89bf173801f5e5bf7921d5ec6eaceeff69c
Marcel Weber <mrweber@gmx.ch>
2022-10-23 16:02:51 +0200

    Working Project display

--- pages/projekte.js
+++ pages/projekte.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-
+import Reel from "../components/Reel"
 function Page_Websites(){
 
     const background = "https://cdn.pixabay.com/photo/2013/02/15/10/58/wave-81840_960_720.jpg"
@@ -7,7 +7,7 @@ function Page_Websites(){
     return(
         <main className="main" style={{backgroundImage: `url("${background}")`}}>
             <section className="section">
-                Projekte
+                <Reel />
             </section>
         </main>
     )
