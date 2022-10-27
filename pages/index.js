ac329185eba43150ebb95f8e78bdcc79e0d0b6fe
Marcel Weber <mrweber@gmx.ch>
2022-10-27 12:54:17 +0200

    Changed classnames for unique frontpage layout

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
 const [prefix, setPrefix] = useState("Vereins")
 
 
-  const background = "/laptop-ga99e6e23c_1920.jpg"
+  const backgroundSplash = "/laptop-ga99e6e23c_1920.jpg"
+  const background = "/neon-lights-7340790_1920.jpg"
 
   return(
     <>
@@ -21,28 +22,15 @@ const [prefix, setPrefix] = useState("Vereins")
         <meta name="description" content="Portfolio Website of Marcel Weber" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main" >
+      <main className="mainFront" style={{backgroundImage: `url("${background}")`}}>
         
-          <article className="splash" style={{backgroundImage: `url("${background}")`}}>
+          <article className="splash" style={{backgroundImage: `url("${backgroundSplash}")`}}>
                           <span className="splashSpan">{`Ihre neue ${prefix}webseite?`}</span>
                           <button className="callToAction">Angebote</button>
                         </article>
-                        <section className="section">
+                        <section className="sectionFront" >
                           <PricingTable />
-                          <h2>Warum mrweber?</h2>
-                          <p>Professionelle Web-Agenturen sind teuer - zurecht! Aber vielfach schlichtweg nicht nötig für eine kleine Vereins-, Hobby- oder KMU-Webseite.</p>
-                          <p>Frei arbeitende Webentwickler und Hobby-Webdesigner wie ich es einer bin arbeiten zumeist mit WordPress. Das ist einerseits schnell, einfach und effizient,
-                            andererseits schaut aber oft alles etwas gleich aus, und dank zusätzlichen Baukstensystemen und Plugins für irgendwelche Zusatzfunktionen sind die Webseiten oft überladen, langsam und irgendwann mühsam zu unterhalten.
-                          </p>
-                          <p>Und was mache ich jetzt anders?</p>
-                          <p>Auch ich nutze gerne WordPress, allerdings lege ich tiefgreifend Hand an und verzichte wo es nur möglich ist auf aufgeblasene Plugins.
-                            Mit HTML, CSS und JavaScript lässt sich WordPress sehr gut über die standardmässigen Optionen hinaus auf die eigenen Bedürfnisse anpassen.
-                          </p>
-                          <p>WordPress ist aber auch nicht immer die beste Wahl. Für eine statische Seite, dessen Inhalt nicht oft ändert, ist eine von Hand gebaute Webseite immer besser.
-                            Aber auch dann, wenn die eigenen Wünsche sehr exotisch sind, kann WordPress einem im Weg stehen.
-                          </p>
-                          <p>Hier kann ich anknüpfen. Ich habe nicht nur Erfahrung mit WordPress, ich kann auch Webseiten von Hand bauen, oder andere Software nutzen. In Über mich sind meine Kompetenzen detailliert dargelegt.</p>        
-        </section>        
+                          </section>        
       </main>
     </>
   )
