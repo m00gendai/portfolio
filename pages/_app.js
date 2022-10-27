a41fc5f98092e110249812886075ce8bf33acb3e
Marcel Weber <mrweber@gmx.ch>
2022-10-27 10:05:45 +0200

    Made footer a component

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,15 @@
 import Component_MainNavMenu from "../components/MainNavMenu"
+import Footer from "../components/Footer"
 import '../styles/globals.css'
 import s from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
 
-  const date = new Date()
-  const foundingYear = "2022"
-  const currentYear = date.getFullYear()
-
   return (
   <>
   <Component_MainNavMenu />
   <Component {...pageProps} />
-  <footer className="footer">
-        © mrweber.ch {foundingYear == currentYear ? "2022" : `2022 - ${currentYear}`}
-      </footer>
+  <Footer />
   </>
   )
 }
