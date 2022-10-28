1e66627f0b30a99a4327e760a4322e7e82077095
Marcel Weber <mrweber@gmx.ch>
2022-10-28 12:59:34 +0200

    Expanded on Angebote text

--- pages/angebote.js
+++ pages/angebote.js
@@ -9,7 +9,7 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
         const [openWPexist, setOpenWPexist] = useState(false)
         const [openCustom, setOpenCustom] = useState(false)
 
-        const background = "" // "https://cdn.pixabay.com/photo/2016/06/04/11/12/particles-1435363_960_720.jpg"
+        const background = "/background-1462755_1920.jpg"
     return(
         <main className="main" style={{backgroundImage: `url("${background}")`}}>
             <section className="section">
@@ -20,34 +20,80 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
                 {openWPnew ?
                 <div className={s.spoiler}>
                     <h4>WordPress Webseite einrichten</h4>
-                    <p>WordPress auf der gewünschten Umgebung installieren und einrichten</p>
-    <h4>Inhalte erstellen</h4>
-<p>Seiten, Texte, Bilder etc. einfügen</p>
-    <h4>Suchmaschinenoptimierung</h4>
-    <p>Damit die Webseite bei Google und Konsorten in den Suchergebnissen vorne mit dabei ist, müssen einige Tricks und 
-        Kniffe angewandt werden. Viele erledigen das mit Plugins für WordPress - diese sind aber oftmals aufgeblasen und funktional eingeschränkt -
-        genau das, was man braucht, erfordert dann Premium-Versionen, die zeitweise sehr viel kosten können.
-        Ich erledige die Optimierung von Hand, nach neuesten Standards. Dazu gehört auch die korrekte Darstellung von Vorschaulinks zB auf sozialen Medien.</p>
-    <h4>Sicherheit</h4>
-        <p>Dies ist ein Bereich, wo ich das Abdecken via Plugin empfehle. Es gibt viele gute Sicherheitsplugins, von gratis bis günstig.</p>
-    <h4>Responsives Design</h4>
-    <p>Die meisten Webseitenaufrufe kommen heutzutage von Mobilgeräten, und dem muss man gerecht werden.</p>
-    <h4>Datenschutz</h4>
-    <p>Die Umsetzung der europäischen Datenschutz Grundverordnung wird eingehalten - dies beinhaltet (leider) auch die nervigen Cookie-Meldungen. DIese werden von mir so wenig störend wie möglich implementiert.</p>
-    <h4>Statistiken</h4>
-    <p>Sehen, wieviele Leute wann ihre Webseite aufrufen und warum und wo sie sie wieder verlassen - Detaillierte Statistiken gehören dazu.</p>
+                    <ul>
+                        <li>WordPress auf der gewünschten Umgebung installieren</li>
+                        <li>Systemeinstellungen vornehmen</li>
+                        <li>Notwendige Plugins installieren & konfigurieren</li>
+                        <li>Sicherheit der Webseite gewährleisten</li>
+                    </ul>
+                    <h4>Inhalte erstellen</h4>
+                    <ul>
+                        <li>Gewünschte Unterseiten erstellen</li>
+                        <li>Texte einbinden (oder neu erstellen)</li>
+                        <li>Bilder einbinden (oder via Bilderanbieter zusammensuchen)</li>
+                    </ul>
+                    <h4>Suchmaschinenoptimierung</h4>
+                    <p>
+                        Damit die Webseite bei Google und Konsorten in den Suchergebnissen vorne mit dabei ist, müssen einige Tricks und 
+                        Kniffe angewandt werden. Viele erledigen das mit Plugins für WordPress - diese sind aber oftmals aufgeblasen und funktional eingeschränkt -
+                        genau das, was man braucht, erfordert dann Premium-Versionen, die zeitweise sehr viel kosten können.
+                        Ich erledige die Optimierung von Hand, nach neuesten Standards. Dazu gehört auch die korrekte Darstellung von Vorschaulinks zB auf sozialen Medien.
+                    </p>
+                    <ul>
+                        <li>Sitemap erstellen</li>
+                        <li>Webseite bei Suchmaschinen indexieren</li>
+                        <li>Korrekte Semantik implementieren</li>
+                        <li>Korrekte Vorschaulinks für soziale Medien prüfen</li>
+                        <li>Für Barrierefreiheit sorgen</li>
+                        <li>Webseitenchecks (Lighthouse) durchführen</li>
+                        <li>Analytik & Statistik einbinden</li>
+                    </ul>
+                    <h4>Sicherheit</h4>
+                    <p>
+                        Dies ist ein Bereich, wo ich das Abdecken via Plugin empfehle. 
+                        Es gibt viele gute Sicherheitsplugins, von gratis bis günstig.
+                    </p>
+                    <ul>
+                        <li>Installieren & konfigurieren von Sicherheitsplugins</li>
+                        <li>Zusätzliche Sicherheitsmassnahmen seitens Hoster vornehmen</li>
+                    </ul>
+                    <h4>Responsives Design</h4>
+                    <p>
+                        Die meisten Webseitenaufrufe kommen heutzutage von Mobilgeräten, dem muss man gerecht werden.
+                        WordPress bietet dies in der Regel von Haus aus, allerdings müssen vor allem bei individuellen Elementen
+                        die Einstellungen von Hand vorgenommen werden
+                    </p>
+                    <ul>
+                        <li>Prüfen der Responsivität</li>
+                        <li>Wo nötig Anpassungen vornehmen</li>
+                    </ul>
+                    <h4>Datenschutz</h4>
+                    <p>
+                        Die Umsetzung der europäischen Datenschutz Grundverordnung wird eingehalten - dies beinhaltet (leider) auch die nervigen Cookie-Meldungen. 
+                        Diese werden von mir so wenig störend wie möglich implementiert.
+                    </p>
+                    <ul>
+                        <li>EU-DSGVO umsetzen</li>
+                        <li>Cookie-Meldung implementieren</li>
+                        <li>Cookie-Handhabung implementieren</li>
+                        <li>Datenschutzdeklaration erstellen</li>
+                        <li>Impressum erstellen</li>
+                    </ul>
                     <h4>Preis</h4>
-                     <p>
-        Sofern Hosting und Domain bereits vorhanden sind, gilt der Preis von
-        CHF500. Ansonsten kommt das Hosting und die Domain noch dazu. Der
-        Gesamtpreis ist dann abhängig vom gewählten Anbieter.
-      </p>
-      </div>
-      :
-      null
+                    <p>
+                        Sofern Hosting und Domain bereits vorhanden sind, gilt der Preis von CHF500. 
+                        Ansonsten kommt das Hosting und die Domain noch dazu. 
+                        Der Gesamtpreis ist dann abhängig vom gewählten Anbieter.
+                    </p>
+                    <p>
+                        In diesem Preis nicht inbegriffen sind etwaige kostenpflichtige Funktionen und Plugins.
+                    </p>
+                </div>
+                :
+                null
                 }
-      <h3 className={s.h3}>WordPress Webseite bestehend</h3>
-        <h3 className={s.h3} onClick={()=> setOpenCustom(!openCustom)}>Massgeschneiderte Lösungen</h3>
+                <h3 className={s.h3}>WordPress Webseite bestehend</h3>
+                <h3 className={s.h3} onClick={()=> setOpenCustom(!openCustom)}>Massgeschneiderte Lösungen</h3>
         {openCustom ?
         <div className={s.spoiler}>
       <p>
